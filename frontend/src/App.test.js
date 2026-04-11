eba55278b3bf5c2aadb49515771ef341ff5a9a49
bcsanup-github <budhathokisanup@gmail.com>
2026-04-11 22:21:31 +0545

    debug app.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DevOps Dashboard heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DevOps Project Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
