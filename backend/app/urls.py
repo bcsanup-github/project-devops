8bfd1ba42526940aef2a994b440dd829f6b0dfe7
bcsanup-github <budhathokisanup@gmail.com>
2026-04-13 18:43:16 +0545

    url maintainance

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,22 +1,13 @@
-"""app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+# A simple view to return a success message
+def api_root(request):
+    return JsonResponse({"status": "success", "message": "Backend API is running perfectly, mate!"})
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('', include('django_prometheus.urls')),
+    path('', api_root, name='api_root'), # This catches the root URL
 ]
