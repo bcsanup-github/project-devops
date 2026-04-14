c402b27d10a3ed2d031ebd45c94b462021766a0c
bcsanup-github <budhathokisanup@gmail.com>
2026-04-14 13:39:05 +0545

    updating entrypoint.prod.sh

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -4,6 +4,7 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
+    # This requires 'netcat-openbsd' which we added to the Dockerfile
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
@@ -11,6 +12,7 @@ then
     echo "PostgreSQL started"
 fi
 
+# Use the 'app' directory we defined as WORKDIR
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input
 
